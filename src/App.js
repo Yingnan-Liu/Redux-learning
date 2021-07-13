8bb168664cf6497c9cb482c5cf745bcedf69abcf
Lynn <yingnanliu.lynn@gmail.com>
2021-07-13 16:32:45 +0800

    redux 最小实践

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default:
+      return state;
+  }
+};
+const store = createStore(counterReducer);
 
 function App() {
+  //使用state的变化诱发渲染
+  const [click, setClick] = useState(false);
+  store.subscribe(() => {
+    const storeNow = store.getState();
+    console.log(storeNow);
+    setClick(!click);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Counter">
+      <div>当前计数值为：{store.getState()}</div>
+      <button onClick={() => store.dispatch({ type: "INCREMENT" })}>+1</button>
+      <button onClick={() => store.dispatch({ type: "DECREMENT" })}>-1</button>
+      <button onClick={() => store.dispatch({ type: "ZERO" })}>0</button>
     </div>
   );
 }
+// 使用render和subscribe诱发渲染
+// const renderApp = () => {
+//   ReactDOM.render(<App />, document.getElementById("root"));
+// };
+// 必须立即调用 renderApp 方法。 没有这个调用，应用程序的第一次渲染将永远不会发生。
+// renderApp();
+// store.subscribe(renderApp);
 
 export default App;
